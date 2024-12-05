00bbbdbd316d6da3fe32685656914981b75b56d0
Prateema Dhakal <prateemadhakal555@gmail.com>
2024-12-05 16:05:44 +0545

    day 5

---
+++ 10_odd_even.py
@@ -0,0 +1,9 @@
+#WAP to find use input number is odd or even
+
+num = int(input("enter number:   "))
+
+if ( num % 2 == 0):
+    print("Even")
+     
+else:
+    print("odd")
--- 9_assignment.py
+++ 9_assignment.py
@@ -3,16 +3,16 @@ marks = int(input("enter your marks   "))
 if isinstance(marks,str) and  not marks.isdigit():
     print("Invalid")
 
-elif marks>40 and marks< 50 :
+elif marks>=40 and marks< 50 :
     print("Pass")
 
-elif marks>50 and marks<60 :
+elif marks>=50 and marks<60 :
     print("Second Division")
 
-elif marks>60 and marks< 75:
+elif marks>=60 and marks< 75:
     print("First Division")
 
-elif marks >75 and marks<= 100:
+elif marks >=75 and marks<= 100:
     print("Distinction")               
 
 else :
