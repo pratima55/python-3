b4c25a841a93573ea137822831683704483e614a
Prateema Dhakal <prateemadhakal555@gmail.com>
2024-12-10 16:15:15 +0545

    day 8

---
+++ 15_loop.py
@@ -0,0 +1,24 @@
+#wap to print reverse of "tribhuwanuniversity"
+#university_name= "tribhuvanuniversity"
+#print(len(university_name))
+#method 1 
+# for i in range( 18,-1,-1):
+#     # print(i)
+#     print(university_name[i], end = " ")
+
+#method 2
+# print(university_name[0:5])  #output = tribh
+# print(university_name[::-1])  #output = ytisrevinunavuhbirt # default[ : :-1]
+
+        
+
+
+
+
+
+#wap to print reverse from 25 to 15
+
+# for i in range( 25, 14,-1):
+#         print(i)
+
+
---
+++ 16_nested_loop.py
@@ -0,0 +1,46 @@
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i,j)    
+#####################################
+
+# for fruit in  ["apple","ball"]:   
+#     for animal in ["cat", "dog"]:
+#         print(fruit,animal)
+
+# output : 
+# apple cat
+# apple dog
+# ball cat
+# ball dog        
+
+
+#######################################
+# for i in "AB":
+#     print(i)
+#     for j in "CD":
+#         print(j)
+# output:
+# A
+# C
+# D
+# B
+# C
+# D      
+##############################
+# for i in range(6):
+#     for j in range(i):
+#         print("*",end = "")
+#     print("")    
+
+#############################
+# for i in range(6,1,-1):
+#     for j in range(i):
+#         print("*",end = "")
+#     print("")  
+
+##############################
+
+for i in "abcd":
+    for j in "abcd":
+        print(i,j)
+        
