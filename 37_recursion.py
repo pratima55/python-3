7bf3a8c58da058125d20f3d75fa57696a79d9c48
Prateema Dhakal <prateemadhakal555@gmail.com>
2024-12-20 21:18:17 +0545

    day 15

---
+++ 37_recursion.py
@@ -0,0 +1,34 @@
+# def display(n):
+#     print(n)
+#     if n<100:
+#         display(n+1)
+
+# display(1)        
+
+#wap to print whole number using recursion function
+
+# def whole_num(n):
+#     print(n)
+#     if n<9:
+#         whole_num(n+1)
+# whole_num(1)
+
+
+#WAP TO PRINT SQUARE OF NUMBER
+# def square(n):
+#     print(n)
+#     if n <50:
+#         square(n*n)
+
+# square(1) 
+
+
+
+##########################
+# for n in range(1,51):
+#     sq= n*n
+#     print(sq)
+
+
+
+
---
+++ 38_fibonacci.py
@@ -0,0 +1,33 @@
+#WAP to make fibonacci series upto n term
+
+# def fibo(n):
+
+#     if n==0:
+#         return 0
+#     elif n==1:
+#         return 1
+#     else:
+#         return fibo(n-1)+ fibo(n-2)
+    
+# term = int(input("enter term:"))
+# for i in range (term):
+#     print(f"fibo series: {fibo(i)}")    
+
+
+#####SIMPLE LOGIC
+#Method1
+# a= 0
+# b=1
+# print(a)
+# print(b)
+# for i in range(10):
+#     c= a+b
+#     print(c)
+#     a,b = b,c
+
+#Method2
+# fib =[0,1]
+# for i in range(10):
+#     fib.append(fib[i]+fib[i+1])
+# print(fib)         
+
